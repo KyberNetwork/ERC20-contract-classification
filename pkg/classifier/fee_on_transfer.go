4a5dc374da749418bc47b8b89509e24f2dd74c8d
kyberliem <cyberliem.civil@gmail.com>
2023-09-19 23:23:23 +0700

    add traceTransaction

--- pkg/classifier/fee_on_transfer.go
+++ pkg/classifier/fee_on_transfer.go
@@ -1,9 +1,12 @@
 package classifier
 
 import (
+	"errors"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/rpc"
 
@@ -72,30 +75,109 @@ func (c *Classifier) ReadSlotStorage(txs []*TxFromTransferEvent) (balanceSlot ma
 	return balanceSlotMap
 }
 
-func (c *Classifier) TraceCallAndGetBalance(txs []*TxFromTransferEvent, balanceSlotMap map[common.Address]common.Hash) {
+func (c *Classifier) TraceCallAndGetBalance(contractAddress common.Address, txs []*TxFromTransferEvent, balanceSlotMap map[common.Address]common.Hash) (map[common.Hash]*StateChanges, error) {
 	var (
 		results = make(map[common.Hash]*StateChanges, len(txs))
 	)
+	contract, avail := balanceSlotMap[contractAddress]
+	if !avail {
+		return nil, errors.New("no contract storage slot available")
+	}
 	for _, tx := range txs {
-		var result interface{}
+		from, avail := balanceSlotMap[tx.From]
+		if !avail {
+			continue
+		}
+		to, avail := balanceSlotMap[tx.To]
+		if !avail {
+			continue
+		}
+
+		var opsResult tracingResult
 		err := jsonrpc.DebugTraceTransaction(
 			c.client,
 			tx.TxHash,
 			nil,
-			result,
+			opsResult,
 		)
 		if err != nil {
 			logger.Warnw("failed to to call json rpc ")
 		}
-		// TODO: print out all state diffs based on balanceSlotMap in the form BalanceDiff
-		results[tx.TxHash] = &StateChanges{
-			Contract: nil,
-			From:     nil,
-			To:       nil,
+		sd := &StateChanges{
+			Contract: &BalanceDiff{
+				Address: contractAddress,
+				Before:  nil,
+				After:   nil,
+			},
+			From: &BalanceDiff{
+				Address: tx.From,
+				Before:  nil,
+				After:   nil,
+			},
+			To: &BalanceDiff{
+				Address: tx.To,
+				Before:  nil,
+				After:   nil,
+			},
 		}
+		if eErr := extractBalance(opsResult, sd, from, to, contract); eErr != nil {
+			logger.Warnw("cannot extract balance", "error", eErr)
+			continue
+		}
+
+		results[tx.TxHash] = sd
 	}
+	return results, nil
 }
 
+func extractBalance(opsResult tracingResult, sd *StateChanges, from, to, contract common.Hash) error {
+	for _, op := range opsResult.ops {
+		decoded, err := hexutil.Decode(op.Value)
+		if err != nil {
+			return err
+		}
+		if op.Op == vm.SLOAD {
+			switch op.Address {
+			case from.String():
+				if sd.From.Before != nil {
+					logger.Warnw("sd.From.Before is already set", "current value", sd.From.Before.String())
+				}
+				sd.From.Before = big.NewInt(0).SetBytes(decoded)
+			case to.String():
+				if sd.To.Before != nil {
+					logger.Warnw("sd.To.Before is already set", "current value", sd.To.Before.String())
+				}
+				sd.To.Before = big.NewInt(0).SetBytes(decoded)
+			case contract.String():
+				if sd.Contract.Before != nil {
+					logger.Warnw("sd.Contract.Before is already set", "current value", sd.Contract.Before.String())
+				}
+				sd.Contract.Before = big.NewInt(0).SetBytes(decoded)
+			}
+		}
+		if op.Op == vm.SLOAD {
+			switch op.Address {
+			case from.String():
+				if sd.From.After != nil {
+					logger.Warnw("sd.From.After is already set", "current value", sd.From.After.String())
+				}
+				sd.From.After = big.NewInt(0).SetBytes(decoded)
+			case to.String():
+				if sd.To.After != nil {
+					logger.Warnw("sd.To.After is already set", "current value", sd.To.After.String())
+				}
+				sd.To.After = big.NewInt(0).SetBytes(decoded)
+			case contract.String():
+				if sd.Contract.After != nil {
+					logger.Warnw("sd.Contract.After is already set", "current value", sd.Contract.After.String())
+				}
+				sd.Contract.After = big.NewInt(0).SetBytes(decoded)
+			}
+		}
+	}
+
+	return nil
+}
 func (c *Classifier) IsFeeOnTransfer(ercContract common.Address) {
 
 	// filter for transfer tx
